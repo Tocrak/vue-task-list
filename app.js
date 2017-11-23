b63b7013e4795ae0e6b5a349eacbdf3e08b48c6b
David Petryca <david@petryca.com>
2017-11-23 15:29:43 +0100

    Update

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 new Vue({
 
-	el: '#app'
+	el: '#app',
+
+	data: {
+		tasks: [
+			{ do: 'eat', done: true },
+			{ do: 'sleep', done: false }
+		]
+	},
+
+	methods: {
+		addTask: function(event) {
+			this.tasks.push({do: 'read', done: false})
+		}
+	}
 
 });
