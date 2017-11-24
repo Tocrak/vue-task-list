0aed81dee47372c94a0659604af9d4f7a9ff8515
David Petryca <david@petryca.com>
2017-11-24 06:44:00 +0100

    Update

--- app.js
+++ app.js
@@ -4,23 +4,31 @@ new Vue({
 
 	data: {
 		newtask: '',
-		tasks: [
-			{ do: 'eat', done: true },
-			{ do: 'sleep', done: false }
-		]
+		tasks: []
+	},
+
+	mounted: function() {
+		var savedTasks = JSON.parse(localStorage.getItem('tasks'));
+		if(savedTasks !== null) {
+			this.tasks = savedTasks;
+		}
 	},
 
 	methods: {
 		addTask: function(event) {
 			this.tasks.push({do: this.newtask, done: false});
+			localStorage.setItem('tasks', JSON.stringify(this.tasks));
 			this.newtask = '';
 		},
-		removeTask: function(index) {
+		removeTask: function(index, evn) {
+			evn.preventDefault();
 			this.tasks.splice(index, 1);
-			return false;
+			window.localStorage.setItem('tasks', JSON.stringify(this.tasks));
 		},
-		toggleDone: function(index) {
+		toggleDone: function(index, evn) {
+			evn.preventDefault();
 		  this.tasks[index].done = !this.tasks[index].done;
+			window.localStorage.setItem('tasks', JSON.stringify(this.tasks));
 		}
 	}
 
